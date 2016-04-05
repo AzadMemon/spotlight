f5a527b464647aaaa3d61ad6a9617438c44489db
Azad Memon <azad.memon@gmail.com>
2016-04-04 21:53:33 -0400

    Added a basic dropdown that lists all the tabs in all the chrome windows

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+$('.ui.dropdown').dropdown({
+  apiSettings: {
+    responseAsync: function (settings, callback) {
+      chrome.windows.getAll(function(windows) {
+        var openTabs = [];
+
+        for (var i = 0; i < windows.length; i++) {
+          chrome.tabs.getAllInWindow(windows[i].id, function(tabs) {
+            for (var j = 0; j < tabs.length; j++) {
+              openTabs.push({
+                'name': tabs[j].title,
+                'value': tabs[j].url
+              });
+            }
+          });
+        }
+
+        callback({success: true, results: openTabs});
+      });
+    }
+  }
+});
