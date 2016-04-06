cfdcc4855f19e074f2c08f9e4c408070259d48ab
Azad Memon <azad.memon@gmail.com>
2016-04-05 22:46:35 -0400

    updates to way popup works

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,24 +1,43 @@
 'use strict';
 
-$('.ui.dropdown').dropdown({
-  apiSettings: {
-    responseAsync: function (settings, callback) {
-      chrome.windows.getAll(function(windows) {
-        var openTabs = [];
+chrome.windows.getAll(function(windows) {
+  var openTabs = [];
 
-        for (var i = 0; i < windows.length; i++) {
-          chrome.tabs.getAllInWindow(windows[i].id, function(tabs) {
-            for (var j = 0; j < tabs.length; j++) {
-              openTabs.push({
-                'name': tabs[j].title,
-                'value': tabs[j].url
-              });
-            }
-          });
-        }
+  for (var i = 0; i < windows.length; i++) {
+    chrome.tabs.getAllInWindow(windows[i].id, function(tabs) {
+      for (var j = 0; j < tabs.length; j++) {
+        openTabs.push({
+          'name': tabs[j].title,
+          'value': tabs[j].url
+        });
+      }
+    });
+  }
 
-        callback({success: true, results: openTabs});
-      });
-    }
+  // For each open tab create a list node and insert into html
+
+});
+
+chrome.history.search({}, function(historyItems) {
+  var history = [];
+
+  for (var i = 0; i < historyItems.length; i++) {
+    history.push({
+      'name': historyItems[i].title,
+      'value': historyItems[i].url
+    });
+  }
+
+  // Append list node and insert into html
+});
+
+chrome.bookmarks.getRecent(20, function(bookmarks) {
+  for (var i = 0; i < bookmarks.length; i++) {
+    bookmarks.push({
+      'name': bookmarks[i].title,
+      'value': bookmarks[i].url
+    });
   }
 });
+
+// On search we redo everything
