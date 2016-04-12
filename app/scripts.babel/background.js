d8edb1a6aadc391bc3cfbada1d40b2ae9435e8af
Azad Memon <azad.memon@gmail.com>
2016-04-11 23:22:51 -0400

    Added on update and on activated listener and listern for arrow down and up

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -3,3 +3,20 @@
 chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
+
+var screenshots = {};
+
+chrome.tabs.onUpdated.addListener(onTapUpdated);
+chrome.tabs.onActivated.addListener(onActivated);
+
+function onTapUpdated(tabId) {
+  chrome.tabs.captureVisibleTab(null, {}, function (image) {
+    screenshots[tabId] = image;
+  });
+}
+
+function onActivated(activeInfo) {
+  chrome.tabs.captureVisibleTab(null, {}, function (image) {
+    screenshots[activeInfo.tabId] = image;
+  });
+}
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,126 +1,78 @@
 'use strict';
 
-// TODO: Add a loading bar in the list when loading search results
-// TODO: On search, need to actually search history and bookmarks and replace contents with that
-// TODO: Need to listen on tab change and take a screenshot chrome.tabs.onActivated
 // TODO: Need to add listener for onEnter to actually select the tab and go to it
-// TODO: Need to add listener to onScroll to a) change image and b) check if image is either last or first and scroll menu all the way to the bottom or top respectively
 // TODO: Sort open tabs by most recently visited
-// TODO: (DON'T NEED BECAUSE POPUP CLOSES ON BLUR) Need to listen on new tab creation chrome.tabs.onCreated and add to list
-// TODO: (DON'T NEED BECAUSE POPUP CLOSES ON BLUR) Need to listen on tab deletion chrome.tabs.onRemoved and remove from list
 
-appendHeading('open');
-addOpenTabsToMenu(function (numberOfResults) {
-  if (numberOfResults === 0) {
-    $('.menu').remove('.open');
-  }
+var counter = 0;
 
-  appendHeading('history');
-  addHistoryToMenu(function (numberOfResults) {
-    if (numberOfResults === 0) {
-      $('.menu').remove('.history');
-    }
+window.addEventListener('keydown', onKeyDown);
 
-    appendHeading('bookmarks');
-    addBookmarksToMenu(function (numberOfResults) {
-      if (numberOfResults === 0) {
-        $('.menu').remove('.bookmarks');
-      }
-    });
-  });
-});
+renderMenu();
+$('input').on('input', renderMenu);
+$('input').focus();
 
-function appendHeading(text) {
-  $('.list').append(
-    '<div class="heading ' + text + '">' + text + '</div>' +
-    '<div class="ui divider ' + text + '"></div>'
-  );
+
+function renderMenu(event) {
+  var text = (event && event.target.value) || '';
+  addOpenTabsToMenu(text.toLowerCase());
 }
 
-function addOpenTabsToMenu(callback) {
+function addOpenTabsToMenu(text) {
   chrome.windows.getAll(function (windows) {
-    var numberOfOpenTabs = 0;
+    var html = '';
+    var tabCounter = 0;
+
     for (var i = 0; i < windows.length; i++) {
       chrome.tabs.getAllInWindow(windows[i].id, function (tabs) {
-        numberOfOpenTabs += tabs.length;
-
         for (var j = 0; j < tabs.length; j++) {
+          if (text && tabs[j].url.indexOf(text) == -1 && tabs[j].title.indexOf(text) == -1) {
+            continue;
+          }
+
           var showDefaultFavIcon = !tabs[j].favIconUrl || tabs[j].favIconUrl.indexOf('chrome://') != -1;
           var favIconUrl = showDefaultFavIcon ? '/images/default-favicon.png' : tabs[j].favIconUrl;
           var title = tabs[j].title || tabs[j].url;
 
-          $('.list').append(
-            '<div class="item">' +
+          html +=
+            '<div id="' + counter + '" class="item" data-tab-id="' + tabs[j].id + '" tabindex="' + tabCounter + '">' +
             '<object class="ui avatar image favicon" data="' + favIconUrl + '" type="image/png">' +
             '<img class="ui avatar image" src="/images/default-favicon.png" />' +
             '</object>' +
             '<div class="content">' +
             '<a class="header">' + title + '</a>' +
             '</div>' +
-            '</div>'
-          );
+            '</div>';
+
+          tabCounter++;
         }
 
-        if (i === windows.length) {
-          console.log(numberOfOpenTabs);
-          callback(numberOfOpenTabs);
-        }
+        $('.list').html(html);
+        $('.list').remove('#' + counter++); // Remove after so we don't get flash
+        $('#0').addClass('selected');
       });
     }
   });
 }
 
-function addHistoryToMenu(callback) {
-  chrome.history.search({text: '', maxResults: 20}, function (history) {
-    for (var j = 0; j < history.length; j++) {
-      var parsedUrl = parseUrl(history[j].url);
-      var showDefaultFavicon = !history[j].url || history[j].url.indexOf('chrome://') != -1;
-      var favIconUrl = showDefaultFavicon ? '/images/default-favicon.png' : parsedUrl.protocol + '//' + parsedUrl.host + '/favicon.ico';
-      var title = history[j].title || history[j].url;
+function onKeyDown(event) {
+  var selectedItem = $('.selected');
+  var index = parseInt(selectedItem.attr('tabindex'));
 
-      $('.list').append(
-        '<div class="item">' +
-        '<object class="ui avatar image favicon" data="' + favIconUrl + '" type="image/png">' +
-        '<img class="ui avatar image" src="/images/default-favicon.png" />' +
-        '</object>' +
-        '<div class="content">' +
-        '<a class="header">' + title + '</a>' +
-        '</div>' +
-        '</div>'
-      );
+  // TODO: Get image from array and insert in here as well
+  // TODO: Need to scroll list so selected is always in view
+  if (event.keyCode === 38) { //Up arrow
+    index -= 1;
+
+    if ($('[tabindex=' + index + ']').length) {
+      selectedItem.removeClass('selected');
+      $('[tabindex=' + index + ']').addClass('selected');
     }
+  } else if (event.keyCode === 40) {
+    index += 1;
 
-    callback(history.length);
-  });
-}
-
-function addBookmarksToMenu(callback) {
-  chrome.bookmarks.getRecent(20, function (bookmarks) {
-    for (var j = 0; j < bookmarks.length; j++) {
-      var parsedUrl = parseUrl(bookmarks[j].url);
-      var showDefaultFavicon = !bookmarks[j].url || bookmarks[j].url.indexOf('chrome://') != -1;
-      var favIconUrl = showDefaultFavicon ? '/images/default-favicon.png' : parsedUrl.protocol + '//' + parsedUrl.host + '/favicon.ico';
-      var title = bookmarks[j].title || bookmarks[j].url;
-
-      $('.list').append(
-        '<div class="item">' +
-        '<object class="ui avatar image favicon" data="' + favIconUrl + '" type="image/png">' +
-        '<img class="ui avatar image" src="/images/default-favicon.png" />' +
-        '</object>' +
-        '<div class="content">' +
-        '<a class="header">' + title + '</a>' +
-        '</div>' +
-        '</div>'
-      );
+    if ($('[tabindex=' + index + ']').length) {
+      selectedItem.removeClass('selected');
+      $('[tabindex=' + index + ']').addClass('selected');
     }
-
-    callback(bookmarks.length);
-  });
-}
-
-function parseUrl(url) {
-  var parser = document.createElement('a');
-  parser.href = url;
-
-  return parser;
+  }
 }
